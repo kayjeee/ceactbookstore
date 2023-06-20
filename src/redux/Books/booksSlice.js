690727b73b4fc4c86e0ddd30df5182a95962cfa0
kayjee <kagiso.killagram@gmail.com>
2023-06-20 10:03:23 +0200

    installed redux

---
+++ src/redux/Books/booksSlice.js
@@ -0,0 +1,17 @@
+import { createSlice } from '@reduxjs/toolkit';
+
+// Books Slice
+const booksSlice = createSlice({
+    name: 'books',
+    initialState: [],
+    reducers: {
+        // Reducer to add a book
+        addBook: (state, action) => {
+            state.push(action.payload);
+        },
+        // Reducer to remove a book
+        removeBook: (state, action) => state.filter((book) => book.id !== action.payload),
+    },
+});
+
+export default booksSlice;
